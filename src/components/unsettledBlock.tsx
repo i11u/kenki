45f3c25918033155c4b1c95104e51aa027f4737a
teru <shingekinoengineer@gmail.com>
2022-07-05 08:34:57 +0900

    Create contenteditable for unsettledBlock

--- src/components/unsettledBlock.tsx
+++ src/components/unsettledBlock.tsx
@@ -1,8 +1,26 @@
 import styled from 'styled-components'
 import { CELL_LENGTH } from '../constants/pageSize'
-import { UnsettledBlock, unsettledBlockSelectors } from '../states/unsettledBlock'
+import { UnsettledBlock, unsettledBlockAction, unsettledBlockSelectors } from '../states/unsettledBlock'
 
 const StyledUnsettledBlock = styled.div`
+  min-height: ${CELL_LENGTH - 1}px;
+  min-width: ${CELL_LENGTH - 1}px;
+  font-size: ${CELL_LENGTH - 1}px;
+  text-align: center;
+  outline: none;
+  margin: -1px 0 0 -1px;
+  border: 2px solid gray;
+  opacity: 0.5;
+  background-color: white;
+  border-radius: 3px;
+  position: absolute;
+  resize: none;
+  z-index: 0;
+  line-height: 1;
+  white-space: nowrap;
+`
+
+const StyledUnsettledBlockCursor = styled.div`
   min-height: ${CELL_LENGTH - 1}px;
   min-width: ${CELL_LENGTH - 1}px;
   font-size: ${CELL_LENGTH - 1}px;
@@ -33,6 +51,7 @@ type Props = {
  * */
 function UnsettledBlockJSX({ id }: Props) {
   const unsettledBlock = unsettledBlockSelectors.useUnsettledBlockById(id) as UnsettledBlock
+  const changeUnsettledBlockPos = unsettledBlockAction.useChangeUnsettledBlockPos()
 
   /**
    * BlockがcontentEditableではなく、BlockはcontentEditableを持つ、という風にした方が使い勝手が良い可能性がある。
@@ -46,7 +65,20 @@ function UnsettledBlockJSX({ id }: Props) {
     height: `${unsettledBlock.block.height * (CELL_LENGTH - 1)}px`,
   }
 
-  return <StyledUnsettledBlock id={`cursor-${id}`} style={style} contentEditable />
+  const style2
+
+  const handleOnInput = () => {
+    const cursor = document.getElementById(`unsettled-block-${id}-cursor`) as HTMLDivElement
+    const newPosition = getNewU
+    changeUnsettledBlockPos(id, newPosition)
+  }
+
+  return (
+    <>
+      <StyledUnsettledBlock id={`unsettled-block-${id}`} style={style} contentEditable onInput={handleOnInput} />
+      <StyledUnsettledBlockCursor id="unsettled-block-${id}-cursor" style={style2} contentEditable />
+    </>
+  )
 }
 
 export default UnsettledBlockJSX
